51e5cfaf7ca975a527103e32b565ad210817d201
zul <salamizulkifli23@icloud.com>
2024-01-11 17:21:49 -0500

    Create Event Files

---
+++ app/Events/BadgeUnlocked.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class BadgeUnlocked
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $badgeName;
+    public $user;
+
+    public function __construct($badgeName, User $user)
+    {
+        $this->badgeName = $badgeName;
+        $this->user = $user;
+    }
+}
