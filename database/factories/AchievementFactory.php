937962c5574b60167ef70a3ae9714face1c5d2e9
zul <salamizulkifli23@icloud.com>
2024-01-12 23:08:26 -0500

    Create Achievement Factory

---
+++ database/factories/AchievementFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Achievement;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AchievementFactory extends Factory
+{
+    protected $model = Achievement::class;
+
+    public function definition()
+    {
+        $achievementNames = [
+            'First Lesson Watched',
+            '5 Lessons Watched',
+            '10 Lessons Watched',
+            '25 Lessons Watched',
+            '50 Lessons Watched',
+            'First Comment Written',
+            '3 Comments Written',
+            '5 Comments Written',
+            '10 Comments Written',
+            '20 Comments Written'
+        ];
+
+        return [
+            'user_id' => User::factory(),
+            'name' => $this->faker->randomElement($achievementNames),
+        ];
+    }
+}
